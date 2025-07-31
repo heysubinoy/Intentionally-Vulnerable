b7d25d4584640a06f7f4caa02926015f9a6af780
Vicky Dewangan <85579798+Vickydew1@users.noreply.github.com>
2025-07-31 18:05:05 +0530

    Create security-group.tf

---
+++ iac/security-group.tf
@@ -0,0 +1,16 @@
+resource "aws_security_group" "insecure_sg" {
+  name        = "allow_ssh_world"
+  description = "Allows SSH from anywhere"
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # ❌ Intentionally insecure
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
