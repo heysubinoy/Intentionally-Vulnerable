af9f16d4a59f8cdcf5997acf2963bb53cd16625d
Vicky Dewangan <85579798+Vickydew1@users.noreply.github.com>
2025-07-31 18:05:33 +0530

    Create app.js

---
+++ container-app/app.js
@@ -0,0 +1,11 @@
+const http = require('http');
+const PORT = 8080;
+
+const requestHandler = (req, res) => {
+  res.end('Hello from AccuKnox demo container!');
+};
+
+const server = http.createServer(requestHandler);
+server.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
